12ce5437d69dac311b35585d97cbbf70a1b0e1ca
shivakumar2006 <official.shivakumar06@gmail.com>
2024-07-21 18:17:40 +0530

    update files

---
+++ src/pages/Authentication.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Logo } from "../assets";
+import { Footer } from "../containers";
+import AuthButtonWithProvider from "../components/AuthButtonWithProvider";
+
+import { FaGoogle, FaGithub } from "react-icons/fa6";
+
+const Authentication = () => {
+  return (
+    <div className="auth-section">
+      <img src={Logo} className="w-12 h-auto object-contain" alt="" />
+
+      <div className="w-full flex flex-1 flex-col items-center justify-center gap-6">
+        <h1 className="text-3xl lg:text-4xl text-blue-700">
+          Welcome to Expressume
+        </h1>
+        <p className="text-base text-gray-600">express way to create resume</p>
+        <h2 className="text-2xl text-gray-600">Authenticate</h2>
+        <div className="w-full lg:w-96 rounded-md p-2 flex-col items-center justify-start gap-6">
+          <AuthButtonWithProvider
+            Icon={FaGoogle}
+            Label={"Signin with Google"}
+            Provider={"GoogleAuthProvider"}
+          />
+          <AuthButtonWithProvider
+            Icon={FaGithub}
+            Label={"Signin with Github"}
+            Provider={"GitHubAuthProvider"}
+          />
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Authentication;
